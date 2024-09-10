05ddb9096bab593c5f3558e50785bc667ab8aa49
Bee-aitch <brayden.huang@mail.utoronto.ca>
2024-09-09 20:33:25 -0400

    Added Reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,18 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int count = 0;
+
+        while (n > 0){
+            if (n % 2 == 0){ // halving even numbers
+                n = n / 2;
+            } else { // subtract 1 otherwisde
+                n -= 1;
+            }
+
+            count++;
+        }
+
+        System.out.println("Steps that took: " + count);
+    }
 }
